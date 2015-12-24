5cf0720e1ca04543ac68cae70428886e5d8ebdb3
haredega <hgarcia.ee@gmail.com>
2015-12-23 22:24:01 -0200

    Adds Break Time
    Implementation of the 'break time' program

---
+++ break-time.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+import msvcrt
+
+while (True):
+    raw_input("Press Enter to continue...")
+    time.sleep(10)
+    webbrowser.open("www.google.com")
+    print 'Did you take a break? Press any button...'
+    
+    
+print "Finish!"
+
